68571460830742b99ff0163dafdc8f65e7e19f41
Chinmay Shet <chishxd@gmail.com>
2025-12-02 01:13:59 +0530

    THE CREATION

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kasumi
+
+*Japanese Music Plays*
+Hello fellow ricers! Are y'all tired of some ugly ahh Local music player too?
+or Are are you afraid of setting up TUI music applications?
+
+Well I present y'all with **KASUMI** a music player every ricer dreams of.
+
+## Planned Features
+
+- Nice looking UI
+- Lightweight in Size
+- Support Real-Time Lyrics
+- Free and Open Source
+- Party Mode!!
---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<main class="container">
+  <h1>Welcome to Kasumi</h1>
+
+  <div class="row">
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+    </a>
+    <a href="https://tauri.app" target="_blank">
+      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank">
+      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+    </a>
+  </div>
+  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+
+  <form class="row" onsubmit={greet}>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button type="submit">Greet</button>
+  </form>
+  <p>{greetMsg}</p>
+</main>
+
+<style>
+.container{
+  text-align: center;
+  padding: 50px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 16px;
+  margin: 20px;
+  height: 90vh;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+h1{
+  color: white;
+}
+
+:global(body){
+  background-color: transparent !important;
+  margin: 0;
+  font-family: sans-serif;
+  overflow: hidden;
+}
+
+:global(html){
+  background-color: transparent !important;
+}
+
+</style>
