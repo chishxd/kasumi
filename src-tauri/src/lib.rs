c0280719d6f6728efbdb8e9155cffd0e560e7365
Chinmay Shet <chishxd@gmail.com>
2025-12-02 16:50:19 +0530

    update run() function. Define Structs and Sinks

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,13 @@
+use rodio::{Decoder, OutputStream, OutputStreamBuilder, Sink};
+use std::fs::File;
+use std::io::BufReader;
+use std::sync::Mutex;
+use tauri::State;
+
+struct AudioState {
+    sink: Mutex<Sink>,
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,7 +16,16 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let stream = OutputStreamBuilder::open_default_stream().unwrap();
+
+    let stream_ptr = Box::leak(Box::new(stream));
+
+    let sink = Sink::connect_new(&stream_ptr.mixer());
+
     tauri::Builder::default()
+        .manage(AudioState{
+            sink: Mutex::new(sink)
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
