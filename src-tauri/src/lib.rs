f5738742589abaaa2d134156e571e0dde0b75e25
Chinmay Shet <chishxd@gmail.com>
2025-12-02 17:35:18 +0530

    define function to play music

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,21 @@ struct AudioState {
     sink: Mutex<Sink>,
 }
 
+#[tauri::command]
+fn play_local_music(path: String, state: State<'_, AudioState>) -> Result<(), String> {
+    let sink = state.sink.lock().map_err(|_| "Failed to lock sink")?;
+
+    let file = File::open(&path).map_err(|e| format!("File not found: {}", e))?;
+    let reader = BufReader::new(file);
+
+    let source = Decoder::new(reader).map_err(|e| format!("Codec error: {}", e))?;
+
+    sink.append(source);
+    sink.play();
+
+    Ok(())
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -23,11 +38,11 @@ pub fn run() {
     let sink = Sink::connect_new(&stream_ptr.mixer());
 
     tauri::Builder::default()
-        .manage(AudioState{
-            sink: Mutex::new(sink)
+        .manage(AudioState {
+            sink: Mutex::new(sink),
         })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, play_local_music])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
